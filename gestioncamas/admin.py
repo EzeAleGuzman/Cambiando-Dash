4ec486ddf272076c6eda99d0daa9ad50dd3b3b3d
EzeAleGuzman <ezealeguzman@gmail.com>
2025-01-30 12:44:15 +0000

    agrego

--- gestioncamas/admin.py
+++ gestioncamas/admin.py
@@ -2,6 +2,10 @@ from django.contrib import admin
 from .models import Servicio, Ubicacion, Cama, PacienteCama
 
 
+class CamasInline(admin.TabularInline):
+    model = Cama
+    extra = 2
+
 class ServicioAdmin(admin.ModelAdmin):
     list_display = (
         "nombre",
@@ -22,6 +26,7 @@ class ServicioAdmin(admin.ModelAdmin):
         "nivel_alerta_ocupacion",
     )
     ordering = ("nombre",)
+    
 
 
 class UbicacionAdmin(admin.ModelAdmin):
@@ -29,13 +34,19 @@ class UbicacionAdmin(admin.ModelAdmin):
     list_filter = ("activo",)
     search_fields = ("nombre", "tipo", "capacidad")
     ordering = ("nombre",)
+    inlines = [CamasInline]
 
 
 class CamaAdmin(admin.ModelAdmin):
-    list_display = ("numero", "estado", "activo", "ubicacion", "notas")
+    list_display = ("numero", "estado", "activo", "ubicacion",'get_servicio',"notas")
     list_filter = ("activo",)
     search_fields = ("numero", "estado")
-    ordering = ("numero",)
+    ordering = ('ubicacion__servicio__nombre', "ubicacion__nombre", "numero")
+
+    def get_servicio(self, obj):
+        return obj.ubicacion.servicio.nombre
+    get_servicio.short_description = 'Servicio'
+    get_servicio.admin_order_field = 'ubicacion__servicio__nombre'
 
 class PacienteCamaAdmin(admin.ModelAdmin):
     list_display = ("paciente", "cama", "fecha_liberacion", "fecha_asignacion")
